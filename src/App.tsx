a3bca8d3db07d8edcbb5a48e22c60b3698189d6a
DeflixFinance <owner@deflix.finance>
2021-12-31 11:53:02 +0100

    link to docs

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Exchange from './components/Exchange'
 import Header from './components/Header'
 import BigNumber from 'bignumber.js'
 import useEagerConnect from 'hooks/useEagerConnect'
+import styled from "styled-components"
 import { usePollFarmsPublicData } from 'state/autofarms/hooks'
 import { Flex } from 'uikit'
 import SocialLinks from 'components/SocialLinks/SocialLinks'
@@ -14,16 +15,28 @@ BigNumber.config({
   DECIMAL_PLACES: 80,
 })
 
+const DocsDiv = styled.div`
+text-align: center;
+margin-top: 5px;
+& > a {
+  color: white;
+}
+& > a:hover {  
+  font-size: 18px
+}
+`
+
 
 const App: React.FC = () => {
-  useEagerConnect()  
+  useEagerConnect()
 
   return (
     <>
       <Header />
       {/* <Exchange />       */}
-      <AutoFarms />      
+      <AutoFarms />
       <SocialLinks />
+      <DocsDiv style={{ textAlign: "center" }}><a href="https://docs.deflix.finance/">wiki</a></DocsDiv>      
       <div>&nbsp;</div>
     </>
   );
